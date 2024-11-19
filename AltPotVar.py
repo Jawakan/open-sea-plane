a0b1d64b74500551bfcf6456bddaa310c8a7f55c
ExcessAl1b1 <138250691+ExcessAl1b1@users.noreply.github.com>
2024-11-19 15:06:22 -0600

    Update AltPotVar.py
    I added a few more potion recipes.
    I made the formatting of printed recipes sexier.
    And I added invalid input for unassociated values, the script will run until manually exited.

--- AltPotVar.py
+++ AltPotVar.py
@@ -1,7 +1,7 @@
 # Ingredients for potion making
 fish = ['Ebonkoi', 'Prismite', 'Shark Fin', 'Doublecod', 'Damselfish', 'Armored Cavefish', 'Crimson Tigerfish', 'Flarefin Koi', 'Obsidifish', 'Prismite', 'Princess Fish', 'Hemopiranha', 'Specular Fish', 'Stinkfish', 'Variegated Lardfish', 'Chaos Fish', 'Frost Minnow']
 plants = ['Moonglow', 'Daybloom', 'Deathweed', 'Fireblossom', 'Blinkroot', 'Grass Seeds', 'Shiverthorn', 'Waterleaf', 'Mushroom', 'Glowing Mushroom', 'Cactus', 'Coral']
-misc = ['Crispy Honey Block', 'Amber', 'Ladybug', 'Pink Pearl', 'White Pearl', 'Black Pearl', 'Obsidian', 'Gel', 'Pixie Dust', 'Crystal Shard', 'Fallen Star', 'Unicorn Horn', 'Lens']
+misc = ['Crispy Honey Block', 'Amber', 'Ladybug', 'Pink Pearl', 'White Pearl', 'Black Pearl', 'Obsidian', 'Gel', 'Pixie Dust', 'Crystal Shard', 'Fallen Star', 'Unicorn Horn', 'Lens', 'Cobweb']
 fragments = ['Nebula Fragment', 'Solar Fragment', 'Stardust Fragment', 'Vortex Fragment']
 # Ingredients & Potions lists are incomplete
 allpots = ['Crate potion', 'Fishing potion', 'Flipper potion', 'Gills potion', 'Greater Luck potion', 'Lesser Luck potion', 'Life Force potion', 'Luck potion', 'Obsidian Skin potion', 'Sonar potion', 'Water Walking potion', 'Lesser Healing potion', 'Healing potion', 'Lesser Mana potion', 'Greater Healing potion', 'Greater Mana potion']
@@ -21,79 +21,88 @@ allheal = ('\nLesser Healing potion | +50 HP\nBottled Honey | +80 HP\nRestoratio
 allmana = ('\nLesser Mana potion | +50 Mana\nMana potion | +100 Mana\nGreater Mana potion | +200 Mana\nSuper Mana potion | +300 Mana\nStrange Brew | +400 Mana\n')
 
 # List of potions
-cratepot = misc[1], plants[0], plants[6], plants[7]
-fishpot = misc[0], plants[7]
-flipperpot = plants[6], plants[7]
-gillspot = plants[7], plants[-1]
-greaterluckpot = plants[7], misc[2], misc[3]
-lesserluckpot = plants[7], misc[2], misc[4]
-lifeforcepot = fish[1], plants[0], plants[6], plants[7]
-luckpot = plants[7], misc[2], misc[5]
-obsidianskinpot = plants[3], plants[7], misc[-1]
-sonarpot = plants[7], plants[-1]
-waterwalkpot = plants[7], fish[2]
-lesshealpot = plants[8], misc[-5], 'Bottle'
-healpot = allpots[11], plants[9]
-greathealpot = misc[8], misc[9]
-manapot = allpots[13], plants[9]
-superhealpot = allpots[14], fragments[0], fragments[1], fragments[2], fragments[3]
-supermanapot = allpots[15], misc[9], misc[10], misc[11]
-ammorespot = fish[3], plants[0] # Start checking here
-archerypot = plants[1], misc[12]
-battlepot = plants[2], 'Vertebra OR Rotten Chunk'
-biomesight = plants[3], plants[4], plants[0], plants[5]
-builderpot = plants[0], plants[4], plants[6]
-calmingpot = fish[4], plants[1]
+cratepot = (misc[1], plants[0], plants[6], plants[7])
+fishpot = (misc[0], plants[7])
+flipperpot = (plants[6], plants[7])
+gillspot = (plants[7], plants[-1])
+greaterluckpot = (plants[7], misc[2], misc[3])
+lesserluckpot = (plants[7], misc[2], misc[4])
+lifeforcepot = (fish[1], plants[0], plants[6], plants[7])
+luckpot = (plants[7], misc[2], misc[5])
+obsidianskinpot = (plants[3], plants[7], misc[-1])
+sonarpot = (plants[7], plants[-1])
+waterwalkpot = (plants[7], fish[2])
+lesshealpot = (plants[8], misc[-5], 'Bottle')
+healpot = (allpots[11], plants[9])
+greathealpot = (misc[8], misc[9])
+manapot = (allpots[13], plants[9])
+superhealpot = (allpots[14], fragments[0], fragments[1], fragments[2], fragments[3])
+supermanapot = (allpots[15], misc[9], misc[10], misc[11])
+ammorespot = (fish[3], plants[0])
+archerypot = (plants[1], misc[12])
+battlepot = (plants[2], 'Vertebra OR Rotten Chunk')
+biomesight = (plants[3], plants[4], plants[0], plants[5])
+builderpot = (plants[0], plants[4], plants[6])
+calmingpot = (fish[4], plants[1])
+cratepot = (misc[1], plants[0], plants[6], plants[7])
+dangersensepot = (plants[6], misc[13])
+endurancepot = (fish[5], plants[4])
 
 while True:
     inqpot = input('What potion would you like to know how to make? ').lower() # Inquire potion
 
     if inqpot == 'crate':
-        print(cratepot) # Add formatting to make output pretty if needed
+        print(', '.join(cratepot))
     elif inqpot == 'fishing':
-        print(fishpot)
+        print(', '.join(fishpot))
     elif inqpot == 'flipper':
-        print(flipperpot)
+        print(', '.join(flipperpot))
     elif inqpot == 'gills':
-        print(gillspot)
+        print(', '.join(gillspot))
     elif inqpot == ('greaterluck') or inqpot == ('greater luck'):
-        print(greaterluckpot)
+        print(', '.join(greaterluckpot))
     elif inqpot == ('lesserluck') or inqpot == ('lesser luck'):
-        print(lesserluckpot)
+        print(', '.join(lesserluckpot))
     elif inqpot == ('lifeforce') or inqpot == ('life force'):
-        print(lifeforcepot)
+        print(', '.join(lifeforcepot))
     elif inqpot == 'luck':
-        print(luckpot)
+        print(', '.join(luckpot))
     elif inqpot == ('obsidianskin') or inqpot == ('obsidian skin'):
-        print(obsidianskinpot)
+        print(', '.join(obsidianskinpot))
     elif inqpot == 'sonar':
-        print(sonarpot)
+        print(', '.join(sonarpot))
     elif inqpot == ('waterwalking') or inqpot == ('water walking'):
-        print(waterwalkpot)
+        print(', '.join(waterwalkpot))
     elif inqpot == ('lesserhealing') or inqpot == ('lesser healing'):
-        print(lesshealpot)
+        print(', '.join(lesshealpot))
     elif inqpot == ('heal') or inqpot == ('healing'):
-        print(healpot)
+        print(', '.join(healpot))
     elif inqpot == ('greaterhealing') or inqpot == ('greater healing'):
-        print(greathealpot)
+        print(', '.join(greathealpot))
     elif inqpot == ('mana'):
-        print(manapot)
+        print(', '.join(manapot))
     elif inqpot == ('superhealing') or inqpot == ('super healing'):
-        print(superhealpot)
+        print(', '.join(superhealpot))
     elif inqpot == ('supermana') or inqpot == ('super mana'): 
-        print(supermanapot)
+        print(', '.join(supermanapot))
     elif inqpot == ('ammoreservation') or inqpot == ('ammo reservation'):
-        print(ammorespot)
+        print(', '.join(ammorespot))
     elif inqpot == ('archery'):
-        print(archerypot)
+        print(', '.join(archerypot))
     elif inqpot == ('battle'):
-        print(battlepot)
+        print(', '.join(battlepot))
     elif inqpot == ('biomesight') or inqpot == ('biome sight'):
-        print(biomesight)
+        print(', '.join(biomesight))
     elif inqpot == ('build') or inqpot == ('builder'):
-        print(builderpot)
+        print(', '.join(builderpot))
     elif inqpot == ('calm') or inqpot == ('calming'):
-        print(calmingpot)
+        print(', '.join(calmingpot))
+    elif inqpot == ('crate'):
+        print(', '.join(cratepot))
+    elif inqpot == ('dangersense') or inqpot == ('danger sense'):
+        print(', '.join(dangersensepot))
+    elif inqpot == ('endurance'):
+        print(', '.join(endurancepot))
     elif inqpot == ('allheal') or inqpot == ('all heal'):
         print(allheal)
     elif inqpot == ('allmana') or inqpot == ('all mana'):
@@ -107,4 +116,4 @@ while True:
     elif inqpot == ('quit') or inqpot == ('exit'):
         break
     else:
-        quit()
+        print('Invalid input')
